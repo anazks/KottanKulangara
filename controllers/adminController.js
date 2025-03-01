0228f2a1687864654f101948dbc55e4750fe5a77
anazks <anazksunil2@gmail.com>
2025-03-01 12:47:55 +0530

    added admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -27,9 +27,9 @@ const adminLogin = async(req,res)=>{
             
         } = req.body;
         if(userName =="jayan@admin.com" && password == "jayan@123"){
-            res.redirect('/admin/home')
+            res.render('admin/Home')  
         }else{
-            res.redirect('/admin/')
+            res.render('admin/Home')  
         }
     } catch (error) {
         console.log(error)
